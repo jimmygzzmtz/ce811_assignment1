753c576ed684119e64a25e63beb5b9dc8236cdc8
Jaime González <jimmygzzmtz@gmail.com>
2019-11-04 17:42:04 +0000

    Cleaned up code and added descriptions
    Also added the scikitlearn jupyter notebook

--- bots/JimmyBotRuleBased.py
+++ bots/JimmyBotRuleBased.py
@@ -6,68 +6,29 @@ import core
 from player import Bot
 
 class JimmyBotRuleBased(Bot):
-    """This is the base class for your AI in THE RESISTANCE.  To get started:
-         1) Derive this class from a new file that will contain your AI.  See
-            bots.py for simple stock AI examples.
-
-         2) Implement mandatory API functions below; you must re-implement
-            those that raise exceptions (i.e. vote, select, sabotage).
-
-         3) If you need any of the optional callback API functions, implement
-            them (i.e. all functions named on*() are callbacks).
-
-       Aside from parameters passed as arguments to the functions below, you 
-       can also access the game state via the self.game variable, which contains
-       a State class defined in game.py.
-
-       For debugging, it's recommended you use the self.log variable, which
-       contains a python logging object on which you can call .info() .debug()
-       or warn() for instance.  The output is stored in a file in the #/logs/
-       folder, named according to your bot. 
-    """
 
     __metaclass__ = core.Observable
 
-    """Version 0.0.6"""
-
+    #variables used to keep track of suspicion on players
     players = []
     leaderSusp = 3
     teamMemberSusp = 2
     votedYesSusp = 1
 
-
     def onGameRevealed(self, players, spies):
-        """This function will be called to list all the players, and if you're
-        a spy, the spies too -- including others and yourself.
-        @param players  List of all players in the game including you.
-        @param spies    List of players that are spies, or an empty list.
-        """
-        #2 spies for 5 players, empty = not a spy
+        #used to give access to these values across the bot's functions
         self.players = players
         self.spies = spies
-        #self.suspicionsArr = [0,0,0,0,0]
 
-        #id, susp
+        #list of lists used to keep track of suspicion, being constantly sorted to save time, in the following format: id, susp
         self.suspicionsBlackboard = [[0,0],[1,0],[2,0],[3,0],[4,0]]
         self.suspicionsBlackboardSort = [[0,0],[1,0],[2,0],[3,0],[4,0]]
 
     def onMissionAttempt(self, mission, tries, leader):
-        """Callback function when a new turn begins, before the
-        players are selected.
-        @param mission  Integer representing the mission number (1..5).
-        @param tries    Integer count for its number of tries (1..5).
-        @param leader   A Player representing who's in charge.
-        """
         pass
 
     def select(self, players, count):
-        """Pick a sub-group of players to go on the next mission.
-        @param players  The list of all players in the game to pick from.
-        @param count    The number of players you must now select.
-        @return list    The players selected for the upcoming mission.
-        """
-         #id, susp
-
+        #Values used to assign team members
         memberRes1 = -1
         memberRes2 = -1
         teamList = []
@@ -91,17 +52,6 @@ class JimmyBotRuleBased(Bot):
                 teamList = [self,players[memberRes1]]
             if(count == 3):
                 teamList = [self,players[memberRes1],players[memberRes2]]
-            
-            """ duplicate debugging
-            print(spyList)
-
-            if(any(spyList.count(x) > 1 for x in spyList)):
-                print(self.spies)
-                print(self.suspicionsBlackboard)
-                print(spyList)
-                print(memberSpy)
-            
-            """
 
             return teamList
         
@@ -126,24 +76,12 @@ class JimmyBotRuleBased(Bot):
             if(count == 3):
                 teamList = [self,players[memberRes1],players[memberRes2]]
 
-            #print(teamList)
             return teamList
 
-            #return [self] + random.sample(self.others(), count - 1)
-
     def onTeamSelected(self, leader, team):
-        """Called immediately after the team is selected to go on a mission,
-        and before the voting happens.
-        @param leader   The leader in charge for this mission.
-        @param team     The team that was selected by the current leader.
-        """
         pass
 
     def vote(self, team):
-        """Given a selected team, decide whether the mission should proceed.
-        @param team      List of players with index and name. 
-        @return bool     Answer Yes/No.
-        """ 
         #always vote yes on the first turn
         if(self.game.turn == 1 and self.game.tries == 1):
             return True
@@ -172,29 +110,17 @@ class JimmyBotRuleBased(Bot):
             return True
 
     def onVoteComplete(self, votes):
-        """Callback once the whole team has voted.
-        @param votes        Boolean votes for each player (ordered).
-        """
         pass
 
     def sabotage(self):
-        """Decide what to do on the mission once it has been approved.  This
-        function is only called if you're a spy, otherwise you have no choice.
-        @return bool        Yes to shoot down a mission.
-        """
+        #don't sabotage if only 2 in team, since it drastically increases supicion, otherwise do sabotage
         if(self.game.team == 2):
             return False
 
         return True 
 
     def onMissionComplete(self, sabotaged):
-        """Callback once the players have been chosen.
-        @param sabotaged    Integer how many times the mission was sabotaged.
-        """
-        
-        #print(self.game.votes)
-
-        #there are spies on the mission
+        #if there are spies on the mission, add suspicion points accordingly
         if(sabotaged != 0):
             for i in range(0, len(self.game.team)):
                 self.suspicionsBlackboard[self.players.index(self.game.team[i])][1] += self.teamMemberSusp
@@ -207,19 +133,14 @@ class JimmyBotRuleBased(Bot):
         self.suspicionsBlackboardSort = sorted(self.suspicionsBlackboard, key=lambda player: player[1])
 
     def onMissionFailed(self, leader, team):
-        """Callback once a vote did not reach majority, failing the mission.
-        @param leader       The player responsible for selection.
-        @param team         The list of players chosen for the mission.
-        """
-        
         spyCheck = False
 
+        #check if a spy, based on suspicion, was probably part of the team
         for i in range(0, len(team)):
                 if(team[i] in self.suspicionsBlackboardSort[((len(self.players)) - (len(team))):((len(self.players)) - 1)] and self.suspicionsBlackboard[i][1] >= 5):
                     spyCheck = True
         
-        #spyCheck = False
-        
+        #if there was probably a spy, add suspicion points accordingly
         if(spyCheck == True):
             self.suspicionsBlackboard[self.players.index(self.game.leader)][1] += self.leaderSusp
 
@@ -230,63 +151,24 @@ class JimmyBotRuleBased(Bot):
         pass
 
     def announce(self):
-        """Publicly state beliefs about the game's state by announcing spy
-        probabilities for any combination of players in the game.  This is
-        done after each mission completes, and takes the form of a mapping from
-        player to float.  Not all players must be specified, and of course this
-        can be innacurate!
-
-        @return dict[Player, float]     Mapping of player to spy probability.
-        """
         return {}
 
     def onAnnouncement(self, source, announcement):
-        """Callback if another player decides to announce beliefs about the
-        game.  This is passed as a potentially incomplete mapping from player
-        to spy probability.
-
-        @param source        Player making the announcement.
-        @param announcement  Dictionnary mapping players to spy probabilities.
-        """
         pass
 
     def say(self, message):
-        """Helper function to print a message in the global game chat, visible
-        by all the other players.
-
-        @param message       String containing free-form text.
-        """
         self.log.info(message)
 
     def onMessage(self, source, message):
-        """Callback if another player sends a general free-form message to the
-        channel.  This is passed in as a generic string that needs to be parsed.
-
-        @param source        Player sending the message.
-        @param announcement  Arbitrary string for the message sent.
-        """
         pass
 
     def onGameComplete(self, win, spies):
-        """Callback once the game is complete, and everything is revealed.
-        @param win          Boolean true if the Resistance won.
-        @param spies        List of only the spies in the game.
-        """
-        #print(self.suspicionsBlackboardSort)
         pass
 
     def others(self):
-        """Helper function to list players in the game that are not your bot."""
         return [p for p in self.game.players if p != self]
 
     def __init__(self, game, index, spy):
-        """Constructor called before a game starts.  It's recommended you don't
-        override this function and instead use onGameRevealed() to perform
-        setup for your AI.
-        @param game     the current game state
-        @param index    Your own index in the player list.
-        @param spy      Are you supposed to play as a spy?
-        """
         super(Bot, self).__init__(self.__class__.__name__, index)
 
         self.game = game
@@ -302,6 +184,5 @@ class JimmyBotRuleBased(Bot):
                 pass
 
     def __repr__(self):
-        """Built-in function to support pretty-printing."""
         type = {True: "SPY", False: "RST"}
         return "<%s #%i %s>" % (self.name, self.index, type[self.spy])
--- bots/JimmyBotSciKitLearn.py
+++ bots/JimmyBotSciKitLearn.py
@@ -5,6 +5,7 @@ import core
 
 from player import Bot
 
+#Used for the SciKitLearn Model
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -12,99 +13,56 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import confusion_matrix, classification_report
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.model_selection import train_test_split
-
 import pickle
 import os
 import csv
 import random
 
 class JimmyBotSciKitLearn(Bot):
-    """This is the base class for your AI in THE RESISTANCE.  To get started:
-         1) Derive this class from a new file that will contain your AI.  See
-            bots.py for simple stock AI examples.
-
-         2) Implement mandatory API functions below; you must re-implement
-            those that raise exceptions (i.e. vote, select, sabotage).
-
-         3) If you need any of the optional callback API functions, implement
-            them (i.e. all functions named on*() are callbacks).
-
-       Aside from parameters passed as arguments to the functions below, you 
-       can also access the game state via the self.game variable, which contains
-       a State class defined in game.py.
-
-       For debugging, it's recommended you use the self.log variable, which
-       contains a python logging object on which you can call .info() .debug()
-       or warn() for instance.  The output is stored in a file in the #/logs/
-       folder, named according to your bot. 
-    """
 
     __metaclass__ = core.Observable
 
-    """Version 0.0.1"""
-
     def onGameRevealed(self, players, spies):
-        """This function will be called to list all the players, and if you're
-        a spy, the spies too -- including others and yourself.
-        @param players  List of all players in the game including you.
-        @param spies    List of players that are spies, or an empty list.
-        """
-        #2 spies for 5 players, empty = not a spy
         self.players = players
         self.spies = spies
 
-        # load the unpickle object into a variable
+        #load the rfc and sc from pickle
         self.rfc = pickle.load(open('rfc_pickle.pkl', 'rb'))
         self.sc = pickle.load(open('sc_pickle.pkl', 'rb'))
 
+        #initialize the lists used to keep track and make predictions of players
         self.blackboard = []
         self.predictionList = []
-
+        
+        #read the list of players to be able to assign scalar values to them
         playerList = []
-
         with open('playerList.csv', 'r') as csvfile:
             if(os.stat("playerList.csv").st_size != 0):
                 playerList = list(csv.reader(csvfile))[0]
 
+        #Assign ints to the players for the blackboard, based on the playerList.csv
         for i in range(0, len(players)):
             nameInt = -1
             if(str(players[i])[2:] != 'JimmyBotSciKitLearn'):
                 nameInt = playerList.index(str(players[i])[2:])
+            
             #player, voted yes sabotaged, voted no sabotaged, votes yes succeeded, voted no succeeded, votes yes failed, voted no failed, in team succeeded, in team sabotaged, leader succeeded, leader sabotaged
             playerTemplate = [nameInt,0,0,0,0,0,0,0,0,0,0]
             self.blackboard.append(playerTemplate)
 
     def onMissionAttempt(self, mission, tries, leader):
-        """Callback function when a new turn begins, before the
-        players are selected.
-        @param mission  Integer representing the mission number (1..5).
-        @param tries    Integer count for its number of tries (1..5).
-        @param leader   A Player representing who's in charge.
-        """
         pass
 
     def select(self, players, count):
-        """Pick a sub-group of players to go on the next mission.
-        @param players  The list of all players in the game to pick from.
-        @param count    The number of players you must now select.
-        @return list    The players selected for the upcoming mission.
-        """
-
-        
-        
-         #id, susp
-
+        #Values used to assign team members
         memberRes1 = -1
         memberRes2 = -1
         teamList = []
-
-        #print(players[0])
 
         if(self.spy):
             #find resistance members that predicts to be spy to fool people, if not choose the first that are not chosen yet
             memberRes1 = self
             memberRes2 = self
-
 
             for i in range(0, len(players)):
                 if(self.predictRFC(self,i) == 1 and players[i] not in self.spies):
@@ -134,11 +92,10 @@ class JimmyBotSciKitLearn(Bot):
                 teamList = [self,memberRes1,memberRes2]
         
         else:
-            #include players that are not predicted to be spies
+            #include players that are not predicted to be spies, if not choose players that aren't the player
 
             memberRes1 = self
             memberRes2 = self
-
 
             for i in range(0, len(players)):
                 if(self.predictRFC(self,i) == 0 and self != players[i]):
@@ -167,28 +124,13 @@ class JimmyBotSciKitLearn(Bot):
             if(count == 3):
                 teamList = [self,memberRes1,memberRes2]
             
-        #print(rfc.predict)
-
-        #print(teamList)
         return teamList
-
-        #return random.sample(self.game.players, count)
-
 
 
     def onTeamSelected(self, leader, team):
-        """Called immediately after the team is selected to go on a mission,
-        and before the voting happens.
-        @param leader   The leader in charge for this mission.
-        @param team     The team that was selected by the current leader.
-        """
         pass
 
     def vote(self, team):
-        """Given a selected team, decide whether the mission should proceed.
-        @param team      List of players with index and name. 
-        @return bool     Answer Yes/No.
-        """ 
 
         #always vote yes on the first turn
         if(self.game.turn == 1 and self.game.tries == 1):
@@ -216,26 +158,17 @@ class JimmyBotSciKitLearn(Bot):
             return True
 
     def onVoteComplete(self, votes):
-        """Callback once the whole team has voted.
-        @param votes        Boolean votes for each player (ordered).
-        """
         pass
 
     def sabotage(self):
-        """Decide what to do on the mission once it has been approved.  This
-        function is only called if you're a spy, otherwise you have no choice.
-        @return bool        Yes to shoot down a mission.
-        """
+        #don't sabotage if only 2 in team, since it drastically increases supicion, otherwise do sabotage
         if(self.game.team == 2):
             return False
 
         return True 
 
     def onMissionComplete(self, sabotaged):
-        """Callback once the players have been chosen.
-        @param sabotaged    Integer how many times the mission was sabotaged.
-        """
-
+        #add respective values to blackboard
         for i in range(0, len(self.players)):
             if(sabotaged == True):
                 if(self.game.votes[i] == True):
@@ -256,79 +189,37 @@ class JimmyBotSciKitLearn(Bot):
                 if(self.players[i] == self.game.leader):
                     self.blackboard[i][9] += 1
         
-        #self.updatePredictionList(self)
 
     def onMissionFailed(self, leader, team):
-        """Callback once a vote did not reach majority, failing the mission.
-        @param leader       The player responsible for selection.
-        @param team         The list of players chosen for the mission.
-        """
-        
+        #add respective values to blackboard
         for i in range(0, len(self.players)):
             if(self.game.votes[i] == True):
                 self.blackboard[i][5] += 1
             else:
                 self.blackboard[i][6] += 1
-        
-        #self.updatePredictionList(self)
 
         pass
 
     def announce(self):
-        """Publicly state beliefs about the game's state by announcing spy
-        probabilities for any combination of players in the game.  This is
-        done after each mission completes, and takes the form of a mapping from
-        player to float.  Not all players must be specified, and of course this
-        can be innacurate!
-
-        @return dict[Player, float]     Mapping of player to spy probability.
-        """
         return {}
 
     def onAnnouncement(self, source, announcement):
-        """Callback if another player decides to announce beliefs about the
-        game.  This is passed as a potentially incomplete mapping from player
-        to spy probability.
-
-        @param source        Player making the announcement.
-        @param announcement  Dictionnary mapping players to spy probabilities.
-        """
         pass
 
     def say(self, message):
-        """Helper function to print a message in the global game chat, visible
-        by all the other players.
-
-        @param message       String containing free-form text.
-        """
         self.log.info(message)
 
     def onMessage(self, source, message):
-        """Callback if another player sends a general free-form message to the
-        channel.  This is passed in as a generic string that needs to be parsed.
-
-        @param source        Player sending the message.
-        @param announcement  Arbitrary string for the message sent.
-        """
         pass
 
     def onGameComplete(self, win, spies):
-        """Callback once the game is complete, and everything is revealed.
-        @param win          Boolean true if the Resistance won.
-        @param spies        List of only the spies in the game.
-        """
-        #print(self.predictionList)
-        """
-        if(self.predictRFC(self,0) == 1):
-            print("0 IS A SPY")
-        else:
-            print('0 is not a spy... :(')
-        """
         pass
     
     def updatePredictionList(event, self):
+        #variable used to create the new list composed of data that is ratioed
         ratioData = []
 
+        #for each player in the blackboard, get each ratio and nameInt, and add it to radioData
         for i in range(0, len(self.blackboard)):
             sabotagedRatio = -1
             notSabotagedRatio = -1
@@ -371,9 +262,11 @@ class JimmyBotSciKitLearn(Bot):
             
             ratioData.append(ratioPlayer)
         
+        #update predictionList using the newly generated ratioData
         self.predictionList = ratioData
     
     def predictRFC(event, self, i):
+        #use the rfc, while scaling the data using sc, to predict if the given player is a spy
         sc = StandardScaler()
         self.updatePredictionList(self)
         Xnew = [self.predictionList[i]]
@@ -383,17 +276,9 @@ class JimmyBotSciKitLearn(Bot):
 
 
     def others(self):
-        """Helper function to list players in the game that are not your bot."""
         return [p for p in self.game.players if p != self]
 
     def __init__(self, game, index, spy):
-        """Constructor called before a game starts.  It's recommended you don't
-        override this function and instead use onGameRevealed() to perform
-        setup for your AI.
-        @param game     the current game state
-        @param index    Your own index in the player list.
-        @param spy      Are you supposed to play as a spy?
-        """
         super(Bot, self).__init__(self.__class__.__name__, index)
 
         self.game = game
@@ -409,6 +294,5 @@ class JimmyBotSciKitLearn(Bot):
                 pass
 
     def __repr__(self):
-        """Built-in function to support pretty-printing."""
         type = {True: "SPY", False: "RST"}
         return "<%s #%i %s>" % (self.name, self.index, type[self.spy])
